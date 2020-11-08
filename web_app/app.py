25d0db7d081a3e4341d2f9ce2da653743c1fc380
Jacob Lebowitz <augmentedmode@Jacobs-MacBook-Pro.local>
2020-11-08 13:47:38 -0500

    web app is functional

--- web_app/app.py
+++ web_app/app.py
@@ -6,15 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    with open('ingred_list.pkl', 'rb') as f:
+    with open('static/models/ingred_list.pkl', 'rb') as f:
         ingred_list = pickle.load(f)
     return render_template('home.html', ingred_list=ingred_list)
 
 
-@app.route('/model')
-def model():
-    model = gensim.models.Word2Vec.load("model.model")
-    results = model.wv.most_similar('romain lettuc')
+@app.route('/api/<string:ingedient>')
+def model(ingedient):
+    
+    model = gensim.models.Word2Vec.load("static/models/model.model")
+    results = model.wv.most_similar(ingedient)
     recommended_ingredients = [x[0] for x in results]
-
-    return recommended_ingredients
+    return render_template('ingredient.html', ingedient=ingedient,
+                            recommended_ingredients=recommended_ingredients)
