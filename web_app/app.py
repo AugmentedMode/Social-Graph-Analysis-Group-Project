d005ca1b4bc4ade54caf6c4d90d89ace2cee4324
Jacob Lebowitz <augmentedmode@Jacobs-MacBook-Pro.local>
2020-11-29 18:45:36 -0500

    added some styling to buttons

--- web_app/app.py
+++ web_app/app.py
@@ -22,19 +22,20 @@ with open('static/models/cuisine_obj.pkl', 'rb') as f:
 
 @app.route('/')
 def home():
-    print(cook_book_v3['grill chicken breast'])
     cook_book_json=json.dumps(cook_book_v3)
     cook_book_json = json.loads(cook_book_json)
-    print(cook_book_json['grill chicken breast'])
     return render_template('home.html', ingred_list=ingred_list, cook_book_v2=cook_book_v3, cuisines=cuisines, cook_book_json=cook_book_json)
 
 
 @app.route('/api/<string:ingedient>')
 def model(ingedient):
+    cook_book_json=json.dumps(cook_book_v3)
+    cook_book_json = json.loads(cook_book_json)
     
     model = gensim.models.Word2Vec.load("static/models/model.model")
     results = model.wv.most_similar(ingedient)
     recommended_ingredients = [x[0] for x in results]
     return render_template('ingredient.html', ingedient=ingedient,
                             recommended_ingredients=recommended_ingredients, 
-                            cook_book_v2=cook_book_v2)
+                            cook_book_v2=cook_book_v3,
+                            cuisines=cuisines, cook_book_json=cook_book_json)
