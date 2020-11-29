2e4863fcbb64b4ca384b36ac37172a729b47c309
Jacob Lebowitz <augmentedmode@Jacobs-MacBook-Pro.local>
2020-11-29 18:59:02 -0500

    filtering is now correct

--- web_app/app.py
+++ web_app/app.py
@@ -14,22 +14,22 @@ with open('static/models/ingred_list_v2.pkl', 'rb') as f:
 with open('static/models/cook_book_v2.pkl', 'rb') as f:
     cook_book_v2 = pickle.load(f)
 
-with open('static/models/cook_book_v3.pkl', 'rb') as f:
-    cook_book_v3 = pickle.load(f)
+with open('static/models/cook_book_v4.pkl', 'rb') as f:
+    cook_book_v4 = pickle.load(f)
 
 with open('static/models/cuisine_obj.pkl', 'rb') as f:
     cuisines = pickle.load(f)
 
 @app.route('/')
 def home():
-    cook_book_json=json.dumps(cook_book_v3)
+    cook_book_json=json.dumps(cook_book_v4)
     cook_book_json = json.loads(cook_book_json)
-    return render_template('home.html', ingred_list=ingred_list, cook_book_v2=cook_book_v3, cuisines=cuisines, cook_book_json=cook_book_json)
+    return render_template('home.html', ingred_list=ingred_list, cook_book_v2=cook_book_v4, cuisines=cuisines, cook_book_json=cook_book_json)
 
 
 @app.route('/api/<string:ingedient>')
 def model(ingedient):
-    cook_book_json=json.dumps(cook_book_v3)
+    cook_book_json=json.dumps(cook_book_v4)
     cook_book_json = json.loads(cook_book_json)
     
     model = gensim.models.Word2Vec.load("static/models/model.model")
@@ -37,5 +37,5 @@ def model(ingedient):
     recommended_ingredients = [x[0] for x in results]
     return render_template('ingredient.html', ingedient=ingedient,
                             recommended_ingredients=recommended_ingredients, 
-                            cook_book_v2=cook_book_v3,
+                            cook_book_v2=cook_book_v4,
                             cuisines=cuisines, cook_book_json=cook_book_json)
